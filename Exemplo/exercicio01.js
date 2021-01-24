99828eecb8dd8603c440d516bceac987ba5215ff
Eduardo Eller <eduardo.eller@outlook.com.br>
2021-01-24 16:18:51 -0300

    alterações

---
+++ Exemplo/exercicio01.js
@@ -0,0 +1,16 @@
+//exercicio 01
+console.log('eduardo');
+console.log('daniel');
+var x = 1;
+var y = 2;
+var z = x+y;
+console.log(z);
+
+//aula 02
+console.log('-------------------')
+console.log('eduardo');
+console.log('daniel');
+var x = 2;
+var y = 6;
+var z = x+y;
+console.log(z);
